a8445e5a5d26015c3b7b71aa1e05b7b72b26fdac
tusharshukla-col <tushar.shukla@somaiya.edu>
2025-09-25 12:41:26 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Jenkins + GitHub CI!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
